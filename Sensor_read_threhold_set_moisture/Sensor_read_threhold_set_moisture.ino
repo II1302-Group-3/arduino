67c5ab883c956dc5cac288fef46b3aa63c3266cc
fedorbas <baskin@kth.se>
2023-05-08 16:23:36 +0200

    Added water sensor logic Co-authored-by: FenixAuriga <fenixscott@gmail.com>

--- Sensor_read_threhold_set_moisture/Sensor_read_threhold_set_moisture.ino
+++ Sensor_read_threhold_set_moisture/Sensor_read_threhold_set_moisture.ino
@@ -4,17 +4,19 @@
 dht DHT;
 
 #define DHT11_PIN 7
+#define FALSE 0
+#define TRUE 1
 
-const int relayPin = 8; 
-char buff[10];
-//unsigned char sendData[4];
+const int relayPin = 8;
+unsigned char recieve_buffer[3]; // star bit , 2 bits for int
 unsigned char sendData[6]; // 2 bytes for temp, 2 for humiidity, 2 for moisture
 int index = 0;
 
 double moisture = 0;
 double moisture_threshold = 0;
-double c = 0;
 char print_flag = 0;
+
+char water_off_flag = FALSE;
 
 
 void setup(){
@@ -45,26 +47,40 @@ void setup(){
   }
 
 // Function that executes whenever data is received from master
+
 void receiveEvent(int howMany) {
 
-  while (Wire.available()) { // loop through all but the last
-    //char c = Wire.read(); // receive byte as a character
-  moisture_threshold = Wire.read()*4;
+  while (Wire.available()) { 
+    int i = 0;
+    while(Wire.available()){ // loop through all but the last
+      recieve_buffer[i] = Wire.read();
+      i++;
+    }
+    int num = recieve_buffer[1] | recieve_buffer[2] << 8;
+    if(num > 1000 && num < 2000){
+       water_off_flag = TRUE;
+    }
+    if(num> 2000){
+       water_off_flag = FALSE;
+    }
+    else{
+      moisture_threshold = num;
+    }
   }
-  print_flag = 1;
+  //print_flag = 1;
 }
 
 void loop(){
   int chk = DHT.read11(DHT11_PIN);
   
   moisture = analogRead(2);
-  
-  if(moisture < moisture_threshold){
-    digitalWrite(relayPin, 0x1);
-    delay(500);
-    digitalWrite(relayPin, 0x0);
-    delay(5000);
-  }
+    if(moisture < moisture_threshold && water_off_flag == FALSE){
+      digitalWrite(relayPin, 0x1);
+      delay(500);
+      digitalWrite(relayPin, 0x0);
+      delay(5000);
+    }
+
   /*
   if(print_flag == 1){
     Serial.print("Temperature = ");
@@ -94,10 +110,11 @@ void loop(){
   int moisturesend = (int)moisture;
   sendData[4] = (moisturesend >>8);
   sendData[5] = (moisturesend >>0);
-  Serial.print("moisturesend: ");
+
+  /*Serial.print("moisturesend: ");
   Serial.println(moisturesend);
   Serial.print("moisture: ");
-  Serial.println(moisture);
+  Serial.println(moisture);*/
   /*
   Serial.println(sendData)
   Serial.println(DHT.temperature);
